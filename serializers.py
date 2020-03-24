a3356da1a90fcd966d2db2c6492e370441204eda
Md Nazmul Hasan <farhapartex@gmail.com>
2020-03-24 22:05:52 +0600

    Category Minimal serializer added

--- serializers.py
+++ serializers.py
@@ -1,7 +1,18 @@
 from rest_framework import serializers
 from .models import *
 
+
+class CategoryMinimalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ("id", "name")
+
 class CategorySerializer(serializers.ModelSerializer):
+    parent = CategoryMinimalSerializer(read_only=True)
+    parent_id = serializers.PrimaryKeyRelatedField(source="parent", queryset=Category.objects.all(), write_only=True)
     class Meta:
         model = Category
         fields = "__all__"
+        extra_kwargs = {
+            "parent_id" : {"write_only" : True}
+        }
