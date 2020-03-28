c21abea46e489b6c99521b20fa3716720f3dee15
Md Nazmul Hasan <farhapartex@gmail.com>
2020-03-28 21:03:47 +0600

    custom action added to Django admin

--- admin.py
+++ admin.py
@@ -1,46 +1,50 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from .models import *
+import logging
+
+logger = logging.getLogger(__name__)
+
 # Register your models here.
-def make_category_active(modeladmin, request, queryset):
-    queryset.update(active=True)
 
-def make_category_deactivate(modeladmin, request, queryset):
-    queryset.update(active=False)
-
-def make_archive(modeladmin, request, queryset):
-    queryset.update(archive=True)
-
-def remove_archive(modeladmin, request, queryset):
-    queryset.update(archive=False)
-
-def unpublish_post(modeladmin, request, queryset):
-    queryset.update(published=False)
-
-def publish_post(modeladmin, request, queryset):
-    queryset.update(published=True)
-
-
-
-make_category_active.short_description = "Active selected categories"
-make_category_deactivate.short_description = "Deactivate selected categories"
-make_archive.short_description = "Archive selected post"
-remove_archive.short_description = "Publish selected post from archive"
-publish_post.short_description = "Publish selected post"
-unpublish_post.short_description = "Unpublish selected post"
-
-
+def get_message_bit(rows_updated, model_name):
+    if model_name == "category":
+        message_bit = "1 category was" if rows_updated == 1 else "%s categories were" % rows_updated
+    elif model_name == "post":
+        message_bit = "1 post was" if rows_updated == 1 else "%s posts were" % rows_updated
+    elif model_name == "tag":
+        message_bit = "1 tag was" if rows_updated == 1 else "%s tags were" % rows_updated
+    elif model_name == "comment":
+        message_bit = "1 comment was" if rows_updated == 1 else "%s comment were" % rows_updated
+    
+    return message_bit
 
 
 @admin.register(Media)
 class MediaAdmin(admin.ModelAdmin):
     list_display = ("id", "image","md_image","sm_image", "created_by", "created_at")
 
+
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     fields = (('parent','name'),'active')
     list_display = ("id", "name", "parent", "active" ,"created_by")
-    actions = [make_category_active, make_category_deactivate]
+    actions = ['make_category_active', 'make_category_deactivate']
+
+    def make_category_active(self, request, queryset):
+        rows_updated = queryset.update(active=True)
+        message_bit = "1 category was" if rows_updated == 1 else "%s categories were" % rows_updated
+
+        self.message_user(request, "%s activated successfully." % message_bit)
+    
+    def make_category_deactivate(self, request, queryset):
+        rows_updated = queryset.update(active=False)
+        message_bit = "1 category was" if rows_updated == 1 else "%s categories were" % rows_updated
+        self.message_user(request, "%s deactivated successfully." % message_bit)
+    
+    make_category_active.short_description = "Active selected categories"
+    make_category_deactivate.short_description = "Deactivate selected categories"
+
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
@@ -50,15 +54,38 @@ class TagAdmin(admin.ModelAdmin):
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     fields = (('category','archive', 'published'),('title', 'slug'), 'content','short_content', ('cover_image',))
-    list_display = ("title", "category", "published", "archive", "created_by")
+    list_display = ("title", "category", "published", "archive", "created_by", "created_at")
     search_fields = ['title','category__name','published']
-    list_filter = ('category__name', 'published', 'archive')
-    actions = [make_archive,remove_archive,publish_post,unpublish_post]
+    list_filter = ('category__name', 'published', 'archive','created_at')
+    actions = ['make_archive','remove_archive','publish_post','unpublish_post']
+
+    def make_archive(self, request, queryset):
+        rows_updated = queryset.update(archive=True)
+        self.message_user(request, "%s archived successfully." % get_message_bit(rows_updated,'post'))
+
+    def remove_archive(self, request, queryset):
+        rows_updated = queryset.update(archive=False)
+        self.message_user(request, "%s published from archive successfully." % get_message_bit(rows_updated,'post'))
+
+    def unpublish_post(self, request, queryset):
+        rows_updated = queryset.update(published=False)
+        self.message_user(request, "%s unpublished successfully." % get_message_bit(rows_updated,'post'))
+
+    def publish_post(self, request, queryset):
+        rows_updated = queryset.update(published=True)
+        self.message_user(request, "%s published successfully." % get_message_bit(rows_updated,'post'))
+    
+    make_archive.short_description = "Archive selected post"
+    remove_archive.short_description = "Publish selected post from archive"
+    publish_post.short_description = "Publish selected post"
+    unpublish_post.short_description = "Unpublish selected post"
+
 
 @admin.register(React)
 class ReactAdmin(admin.ModelAdmin):
     list_display = ("id","blog", "type", "amount",)
     search_fields = ['blog__title', 'type']
+
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
